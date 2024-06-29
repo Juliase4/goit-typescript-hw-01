b549c5c50ebc674e53d981db652ac8ccfadf9c3e
Yulia Serheieva <Julia.sergeeva93@gmail.com>
2024-06-30 02:37:20 +0300

    add task generics

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,3 +1,6 @@
+// Типізуйте асинхронну функцію fetchData, яка приймає URL ресурсу та повертає об'єкт з даними.
+// Використовуйте Generics для типізації повернутих даних.
+
 // import axios from "axios";
 
 // async function fetchData(url) {
@@ -8,3 +11,24 @@
 //     throw new Error(`Error fetching from ${url}: ${error}`);
 //   }
 // }
+
+import axios from "axios";
+
+async function fetchData<T>(url: string): Promise<T> {
+  try {
+    const response = await axios.get<T>(url);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching from ${url}: ${error}`);
+  }
+}
+
+interface Data {
+  id: number;
+  name: string;
+}
+
+const url = "https://api.example.com/data";
+fetchData<Data>(url)
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
--- src/generics/6.ts
+++ src/generics/6.ts
@@ -1,3 +1,5 @@
+// У вас є тип Form, який містить інформацію про форму, включаючи поле errors. Ви хочете створити новий тип Params, який включає всі поля з Form, крім errors.
+
 // type Errors = {
 //   email?: string[];
 //   firstName?: string[];
@@ -16,3 +18,20 @@
 // // Реалізуйте Params так,
 // // щоб унеможливити поле 'errors' з типу Form
 // type Params = Form;
+
+type Errors = {
+  email?: string[];
+  firstName?: string[];
+  lastName?: string[];
+  phone?: string[];
+};
+
+type Form = {
+  email: string | null;
+  firstName: string | null;
+  lastName: string | null;
+  phone: string | null;
+  errors: Errors;
+};
+
+type FormWithoutError = Omit<Form, "errors">;
